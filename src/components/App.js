b9d41a17d777f104ef6e3620d331684b7e9f1b41
Andrew Hearse <andy@claroh.com>
2020-10-17 15:39:04 +0100

    project sync

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,19 @@ const App = () => {
 
     return (
         <div>
-            My sandpit to play in!!!
+            <h1>My sandpit to play in!!!</h1>
+            <p>
+                <label htmlFor="country">Country:</label>
+                <select list="countries" id="country" defaultValue="Guernsey">
+                    <option value="Bahamas">Bahamas</option>
+                    <option value="Cayman Islands">Cayman Islands</option>
+                    <option value="Guernsey">Guernsey</option>
+                    <option value="Hong Kong">Hong Kong</option>
+                    <option value="Jamaica">Jamaica</option>
+                    <option value="Jersey">Jersey</option>
+                    <option value="Isle of Man">Isle of Man</option>
+                </select>
+            </p>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
+const countChar = (text, letter) => {
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countChar('kakkerlak', 'k'));
+
+// const size = 8;
+// let chess = '';
+
+// for (let i = 1; i <= size; i++) {
+//     for (let j = 1; j <= size; j++) {
+//         if ((i % 2) !== (j % 2)) {
+//             chess += '#';
+//         } else {
+//             chess += ' ';
+//         }
+//     }
+//     chess += '\n';
+// }
+
+// console.log(chess
+// );
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
